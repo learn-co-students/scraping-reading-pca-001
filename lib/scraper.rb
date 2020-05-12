07239a43726df2347f8c95ddd324b9730dff4904
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-12 14:01:12 -0400

    leson complete

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+puts doc.css(".headline-26OIBN").text
+tmp = doc.css(".container-3Oeaih")[0]
+puts tmp.attributes
